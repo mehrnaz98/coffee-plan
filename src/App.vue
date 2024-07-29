e8f019230f5daaf3ad13eda48b80fd5611095c29
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-29 22:55:18 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script setup></script>
 
 <template>
-  <div></div>
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+
+    <div class="plans">
+      <div class="plan">
+        <div class="description">
+          <span class="title"> The Single </span>
+        </div>
+      </div>
+
+      <div class="plan">
+        <div class="description">
+          <span class="title"> The Curious </span>
+        </div>
+      </div>
+
+      <div class="plan">
+        <div class="description">
+          <span class="title"> The Addict </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
