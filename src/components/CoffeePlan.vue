e65879939c22766781b03acb9c6bb4e307235aef
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-30 22:34:05 +0330

    add function select-plan

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { ref } from "vue";
 defineProps(["name"]);
+
 const selected = ref(false);
+function selectPlan() {
+  selected.value = !selected.value;
+}
 </script>
 
 <template>
-  <div
-    @click="selected = !selected"
-    class="plan"
-    :class="{ 'active-plan': selected }"
-  >
+  <div @click="selectPlan" class="plan" :class="{ 'active-plan': selected }">
     <div class="description">
       <span class="title"> {{ name }}</span>
     </div>
