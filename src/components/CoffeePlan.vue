3e8d5fe4f6aa5a27bc170d1bdd1e639d4013ac90
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-07-30 22:26:05 +0330

    Update CoffeePlan.vue
    at click selected will be unselected and vice versa

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -5,7 +5,11 @@ const selected = ref(false);
 </script>
 
 <template>
-  <div class="plan" :class="{ 'active-plan': selected }">
+  <div
+    @click="selected = !selected"
+    class="plan"
+    :class="{ 'active-plan': selected }"
+  >
     <div class="description">
       <span class="title"> {{ name }}</span>
     </div>
